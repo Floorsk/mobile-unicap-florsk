52f3091635c31200d585e796aee7194b6a6507b2
Florsk <davi.galdino08@gmail.com>
2023-08-26 12:53:54 -0300

    pope francis

--- expo-01/App.js
+++ expo-01/App.js
@@ -1,39 +1,82 @@
 import { useState, useReducer } from 'react'
-import { FlatList, StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Button, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 export default function App() {
-  const [activity, setActivity] = useState();
+
+  const initialState = {
+    data: [],
+    text: '',
+    index: 0,
+  }
 
   const reducer = (state, action) => {
-    if (action.type === 'addActivity') {
-      return {
-        data: [...state.data, activity]
-      }
+
+    switch (action.type) {
+      case 'setText':
+        return { ...state, text: action.payload }
+        break;
+      case 'addActivity':
+        if (state.text === '') {
+          alert('Você precisa digitar algo para adicionar a sua lista!')
+          return { ...state }
+        }
+        return {
+          ...state,
+          data: [...state.data, state.text],
+          text: ''
+        }
+      case 'setIndex':
+        return { ...state, index: action.payload }
+      case 'deleteActivity':
+        return { ...state, data: state.data.filter((item, index) => index != state.index) }
+      default:
+        return state
     }
   }
 
-  const [state, dispach] = useReducer(reducer, { data: [] });
+  const [state, dispach] = useReducer(reducer, initialState);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hey, Let's make a to do list!</Text>
-      <TextInput
-        placeholder='Digite aqui'
-        onChangeText={setActivity}
-        style={styles.input}
-      />
-      <TouchableOpacity style={styles.buttonContainer} onPress={() => dispach({ type: 'addActivity' })}>
-        <Text style={styles.buttonText}>Add Activity</Text>
-      </TouchableOpacity>
-      <FlatList
-        data={state.data}
-        renderItem={({ item }) => (
-          <View style={styles.activityContainer}>
-            <Text>{item}</Text>
-          </View>
-        )}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container} >
+
+        <View style={styles.titleArea}>
+          <Text style={styles.title}>Hey, Let's make a to-do list! 📝</Text>
+        </View>
+
+        <TextInput
+          placeholder='Digite aqui'
+          onChangeText={(newText) => dispach({ type: 'setText', payload: newText })}
+          value={state.text}
+          style={styles.input}
+          cursorColor={'#6F50FF'}
+        />
+
+        <TouchableOpacity style={styles.buttonContainer} onPress={() => dispach({ type: 'addActivity' })}>
+          <Text style={styles.buttonText}>Adicionar item</Text>
+        </TouchableOpacity>
+
+        <FlatList
+          data={state.data}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item, index }) => (
+
+            <View style={styles.activityContainer}>
+              <Text style={styles.activity}>{item}</Text>
+              <TouchableOpacity style={styles.deleteArea} onPress={() => { dispach({ type: 'setIndex', payload: index }); dispach({ type: 'deleteActivity' }) }}>
+                <Feather name="trash" size={20} color="red" />
+              </TouchableOpacity>
+            </View>
+
+          )}
+          ListFooterComponent={() => <View style={styles.footer}/>}
+        />
+        <View style={styles.dataCountArea}>
+          <Text style={styles.buttonText}>Número de itens na lista: {state.data.length}</Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -43,10 +86,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 50
+
+  },
+  titleArea: {
+    width: '100%',
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#6F50FF'
   },
   title: {
+    color: "#FFF",
     fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 10
   },
   input: {
     width: 300,
@@ -54,16 +107,16 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     margin: 10,
     borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 10
+    borderColor: '#C6C6C6',
+    borderRadius: 5
   },
   buttonContainer: {
     width: 200,
     height: 40,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'pink',
-    borderRadius: 10,
+    backgroundColor: '#6F50FF',
+    borderRadius: 5,
     marginBottom: 10,
   },
   buttonText: {
@@ -72,13 +125,38 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   activityContainer: {
-    width: 300,
+    width: 370,
+    height: 70,
+    justifyContent: 'center',
+    marginBottom: 10,
+    paddingLeft: 20,
+    borderWidth: 1,
+    borderColor: '#C9C9C9',
+    borderRadius: 5
+  },
+  activity: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  deleteArea: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    right: 10,
+    borderRadius: 5
+  },
+  dataCountArea: {
+    width: '100%',
     height: 40,
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 10,
-    borderWidth: 1,
-    borderRadius: 10,
-    
+    backgroundColor: '#6F50FF',
+    position: 'absolute',
+    bottom: 0
+  },
+  footer: {
+    height: 40
   }
 });
